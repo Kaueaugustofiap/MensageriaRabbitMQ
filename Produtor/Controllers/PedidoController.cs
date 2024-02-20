54b7ede4b8bbefe0704b3cdab92d1e248c16972f
Kaueaugustofiap <kaueaugusto2002@hotmail.com>
2024-02-20 15:09:54 -0300

    Projeto subido com sucesso.

---
+++ Produtor/Controllers/PedidoController.cs
@@ -0,0 +1,28 @@
+﻿using Core;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Produtor.Controllers
+{
+    [ApiController]
+    [Route("/Pedido")]
+    public class PedidoController : ControllerBase
+    {
+        private readonly IBus _bus;
+        private readonly IConfiguration _configuration;
+
+        public PedidoController(IBus bus, IConfiguration configuration)
+        {
+            _bus = bus;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var endpoint = await _bus.GetSendEndpoint(new Uri("queue:fila"));
+            await endpoint.Send(new Pedido(1,new Usuario(5, "Kaue", "kaue@fiap.com")));
+            return Ok();
+        }
+    }
+}
